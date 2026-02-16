3deffbdfd759a3e48e6860eb1147d8bc66013c6b
papertyga <>
2026-02-16 00:55:26 +0000

    Tuple addition

---
+++ mastering_python_tuples/tuple_addition/main.py
@@ -0,0 +1,6 @@
+animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
+
+# Write your code here
+temp_list = animal_movies + ('Dumbo', 'Zootopia')
+animal_movies = temp_list
+print("Updated animal movies:", animal_movies)
